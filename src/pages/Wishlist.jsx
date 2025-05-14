388ec62c9b9f60bbb50cf1b199ca2b03beef9aff
ebythomas309 <ebythomas309@gmail.com>
2025-05-14 21:41:01 +0530

    wishlist updated

---
+++ src/pages/Wishlist.jsx
@@ -0,0 +1,47 @@
+//rafce
+import React from 'react'
+import Header from '../components/Header'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeItem } from '../redux/slices/wishlistSlice'
+
+const Wishlist = () => {
+  const dispatch = useDispatch()
+  const userwishlist = useSelector(state => state.wishlistReducer)
+  return (
+    <>
+      <Header />
+      <div style={{ paddingTop: '100px' }} className='px-5'>
+        {
+          userwishlist?.length > 0 ?
+            <>
+              <h1 className="text-4xl font-bold text-red-600 ">My Wishlist</h1>
+              <div className="grid grid-cols-4 gap-4 mt-5">
+                {
+                  userwishlist?.map(product => (
+                    <div key={product?.id} className="rounded border p-2 shadow">
+                      <img width={'100%'} height={'200px'} src={product?.thumbnail} alt="" />
+                      <div className="text-center">
+                        <h3 className="text-xl font-bold">{product?.title}</h3>
+                        <div className="flex justify-evenly mt-3">
+                          <button onClick={()=>dispatch(removeItem(product?.id))} className="text-xl"><i className="fa-solid fa-heart-circle-xmark text-red-500"></i></button>
+                          <button className="text-xl"><i className="fa-solid fa-cart-plus text-green-500"></i></button>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            </>
+            :
+            <div className="flex flex-col justify-center item-center ">
+              <img className='w-100 h-1/2' src="https://krosfitsports.com/public/empty-cart.gif" alt="" />
+              <h1 className="text-3xl text-red-600 mt-3">your Wishlist is Empty!!!</h1>
+            </div>
+        }
+      </div>
+
+    </>
+  )
+}
+
+export default Wishlist
---
+++ src/redux/slices/productSlice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//action return promise
+export const fetchProducts = createAsyncThunk("products/fetchProducts",async ()=>{
+ const result = await axios.get("https://dummyjson.com/products")
+//console.log( result.data.products);
+  sessionStorage.setItem("allProducts",JSON.stringify(result.data.products))
+ return result.data.products
+ 
+});
+
+const productSlice= createSlice({
+    name:'products',
+    initialState:{
+        allproducts:[],
+        loading:false,
+        errorMsg:""
+    },
+    reducers:{
+
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchProducts.fulfilled,(state,apiResult)=>{
+            state.allproducts = apiResult.payload
+            state.loading = false
+            state.errorMsg = ""
+        })
+        builder.addCase(fetchProducts.pending,(state,apiResult)=>{
+            state.allproducts = []
+            state.loading = true
+            state.errorMsg = ""
+        })
+        builder.addCase(fetchProducts.rejected,(state,apiResult)=>{
+            state.allproducts = []
+            state.loading = false
+            state.errorMsg = "API Call failed"
+        })
+    }
+})
+
+
+export default productSlice.reducer
